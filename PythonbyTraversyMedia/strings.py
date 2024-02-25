9dd0fba6a8ffa0884e00cb7ca49dbf64d8f5b84e
Uzair16ahmed <khanuzair285@gmail.com>
2024-02-25 15:35:36 +0500

    Strings and their methods.

--- PythonbyTraversyMedia/strings.py
+++ PythonbyTraversyMedia/strings.py
@@ -10,4 +10,62 @@ print('Hello, my name is ' + name +' and i am '+ str(age))
 
 # String Formatting
 
+# Arguments by position
+print('My name is {name} and i am {age}'.format(name=name, age=age))
+
+# F-Strings (3.6+)
+print(f'Hello, my name is {name} and i am {age}')
+
 # String Methods
+
+s = 'heLlo woRld'
+
+# Capitalize the string
+print('Capitalize the string:', s.capitalize())
+
+# Make all uppercase
+print('Make all uppercase:',s.upper())
+
+# Make all lowercase
+print('Make all lower:', s.lower())
+
+# swap case
+print('swap case:', s.swapcase())
+
+# Get length
+print('length of String:', len(s))
+
+# Replace 
+print('replace word:', s.replace('woRld', 'Everyone'))
+
+# Count the specific char in string
+sub = 'e'
+print(f'count of char {sub} in string:', s.count(sub))
+
+# Start with
+print('if string starts with word return true:', s.startswith('heLlo'))
+
+# Ends with
+print('if string ends with word return true:', s.endswith('woRld'))
+print('if string ends with word (s) return true:', s.endswith('s'))
+
+# Split into a list
+print('splits the string into a list:', s.split())
+
+# find the position of the char
+print('position of the char (w) in string:', s.find('w'))
+
+# is all alphanumeric
+print('is all alphanumeric ?:',s.isalnum())
+
+# is all alphabetic
+print('is all alphabetic ?:', s.isalpha())
+
+# is all numeric
+print('is all numeric ?:', s.isnumeric())
+
+
+
+
+
+
