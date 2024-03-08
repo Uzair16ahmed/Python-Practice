ea1ee71d1b06ce7cbb28b4182a588b3b153554b1
Uzair16ahmed <khanuzair285@gmail.com>
2024-03-08 16:44:46 +0500

    completed from 66 to 70

--- from_66_to_70_problems/from_66_to_70_problems.py
+++ from_66_to_70_problems/from_66_to_70_problems.py
@@ -150,3 +150,27 @@ The third-party module developers usually register at PyPI and uploads their pac
 take two arguments. The name of the csv file to read as first argument and the name of the Excel file to write as
 the second argument. 
 """
+
+""" import pandas as pd
+import sys
+
+def csv_to_excel(csv_file, excel_file):
+    try:
+        # Read CSV file into a DataFrame
+        df = pd.read_csv(csv_file)
+        print(df)
+        # Export DataFrame to Excel file
+        df.to_excel(excel_file, index=False)
+        print(f"CSV file '{csv_file}' successfully converted to Excel file '{excel_file}'.")
+    except Exception as e:
+        print(f"Error converting CSV file '{csv_file}' to Excel file '{excel_file}': {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv)!= 3:
+        print("Usage: python csv_to_excel.py <csv_file> <excel_file>")
+        sys.exit(1)
+    else:
+        csv_file = sys.argv[1]
+        excel_file = sys.argv[2]
+        csv_to_excel(csv_file, excel_file) """
