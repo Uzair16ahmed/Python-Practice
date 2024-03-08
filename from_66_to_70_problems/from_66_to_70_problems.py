a4d4ce18847a649cee02abd5c78cd9979aad8b81
Uzair16ahmed <khanuzair285@gmail.com>
2024-03-08 15:56:34 +0500

    getting documentation of a module

--- from_66_to_70_problems/from_66_to_70_problems.py
+++ from_66_to_70_problems/from_66_to_70_problems.py
@@ -104,3 +104,35 @@ if __name__ == "__main__":
 """ Problem 69: Write a program mydoc.py to implement the functionality of pydoc. The program should take
 the module name as argument and print documentation for the module and each of the functions defined in that
 module """
+
+""" 
+import importlib
+import inspect
+import sys
+
+def print_module_doc(module_name):
+    try:
+        module = importlib.import_module(module_name)
+        print(f"Documentation for module:", {module_name})
+        print(module.__doc__)
+        print()
+
+        functions = inspect.getmembers(module, inspect.isfunction)
+        for name, function in functions:
+            print(f"Documentation for function:", {name})
+            print(function.__doc__)
+            print()
+
+    except ImportError:
+        print(f"Module '{module_name}' not found.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python print_module_doc.py <module_name>")
+        sys.exit()
+    else:
+        module_name = sys.argv[1]
+        print_module_doc(module_name)
+
+"""
